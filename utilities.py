d2bc44b01e1f91e515d51a03dd5798a41a25edca
Niko Dalla Noce <81106351+nikodallanoce@users.noreply.github.com>
2021-06-18 18:43:09 +0200

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,10 +1,16 @@
-import numpy as np
-
-
 def create_patterns(name: str):
-    dataset = list()
-    with open(name) as datafile:
-        for row in datafile:
-            dataset.append(row)
+    with open(name, encoding='UTF-8') as datafile:
+        sentences = datafile.readlines()
+    return sentences
 
-    return np.asarray(dataset)
+
+def create_dataset(eng_ita=True):
+    src, dst = 'en', 'it'
+    if not eng_ita:
+        src, dst = 'it', 'en'
+    x = create_patterns('dataset/europarl-v7.it-en.{0}'.format(src))
+    y = create_patterns('dataset/europarl-v7.it-en.{0}'.format(dst))
+    ds = []
+    for (xi, yi) in zip(x, y):
+        ds.append((xi, yi))
+    return ds
