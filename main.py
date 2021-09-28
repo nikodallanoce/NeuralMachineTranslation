c2bcb264fbb96ae46044e5ce5c4ed6e6f84a8f1e
nikodallanoce <dllnik97@gmail.com>
2021-09-28 12:54:31 +0200

    Qualcosina funziona

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from utilities import *
-from keras.preprocessing.text import Tokenizer
-
-
+import tensorflow as tf
+from transformers import BertTokenizer, TFBertModel, BartTokenizer, TFBartModel
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.utils import plot_model
+import tensorflow_addons as tfa
 if __name__ == '__main__':
-    dataset_en = create_patterns("dataset/europarl-v7.it-en.en")
-    dataset_it = create_patterns("dataset/europarl-v7.it-en.it")
-    dataset_en = [sentence.replace("\n", "") for sentence in dataset_en]
-    tokenizer = Tokenizer(num_words=10000)
-    tokenizer.fit_on_texts(dataset_en)
-    tokens = tokenizer.texts_to_sequences(dataset_en)
-    print("Successfully loaded datasets")
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    model: TFBertModel = TFBertModel.from_pretrained("bert-base-uncased", trainable=True)
+    tokens = tokenizer("good job guys", add_special_tokens=True, truncation=True, padding="max_length",
+                        return_attention_mask=True, return_token_type_ids=True, return_tensors="tf", max_length=25)
+    ret = model.call(**tokens, output_attentions=True, output_hidden_states=True)
+    print()
